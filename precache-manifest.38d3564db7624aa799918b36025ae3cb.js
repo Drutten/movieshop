ebde7dc84d3eb83fcab125c968114e1e3541c762
Drutten <rosi.drott@gmail.com>
2020-05-01 22:07:13 +0200

    Updates

---
+++ precache-manifest.38d3564db7624aa799918b36025ae3cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1adc1482b2a1b462699caf1b902591",
+    "url": "/movieshop/index.html"
+  },
+  {
+    "revision": "9824908dcc2725a47b18",
+    "url": "/movieshop/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9aab4029e3d6d051f41e",
+    "url": "/movieshop/static/css/main.01691d8e.chunk.css"
+  },
+  {
+    "revision": "9824908dcc2725a47b18",
+    "url": "/movieshop/static/js/2.b64b2b08.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movieshop/static/js/2.b64b2b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aab4029e3d6d051f41e",
+    "url": "/movieshop/static/js/main.f5888756.chunk.js"
+  },
+  {
+    "revision": "f2ef284c2af52d327fd8",
+    "url": "/movieshop/static/js/runtime-main.2df4ac55.js"
+  },
+  {
+    "revision": "ad6e003eec67782834c6e6dd5f1cee90",
+    "url": "/movieshop/static/media/frulle.ad6e003e.gif"
+  },
+  {
+    "revision": "31b786b9ffe00f5216ce29ba075fd5f1",
+    "url": "/movieshop/static/media/kamera.31b786b9.png"
+  },
+  {
+    "revision": "1227f99f265708dd6ac5f4090c4bef20",
+    "url": "/movieshop/static/media/movies.1227f99f.png"
+  },
+  {
+    "revision": "c4ef64634aa9194d83a479483edce23f",
+    "url": "/movieshop/static/media/profil3.c4ef6463.png"
+  }
+]);
